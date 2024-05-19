3d53e89a08cb30d7c3a25217a9562e5b56c50749
stoweandi <146253464+stein-key@users.noreply.github.com>
2024-05-18 17:31:28 -0700

    vue3 and Vuetify router 4
    Refactored with Vuetify and Vue3 and Vue Router 4
    
    - Page navigation
    - form and feed functionality

--- src/views/AboutView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="about">
-    <h1>Testimonies</h1>
-    <p>Share your testimony</p>
-  </div>
-</template>
--- src/views/ScriptureView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="scripture">
-    <h1>Scripture</h1>
-    <p>Find scripture to encourage you and share with others</p>
-  </div>
-</template>
